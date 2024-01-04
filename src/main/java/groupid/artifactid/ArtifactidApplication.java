2879c5fbfc92db946a76f2a200b33d13b4e6dd11
Carolina Segura <carosegural13@gmail.com>
2024-01-03 20:37:13 -0500

    Funciones de repositorio CRUD

--- src/main/java/groupid/artifactid/ArtifactidApplication.java
+++ src/main/java/groupid/artifactid/ArtifactidApplication.java
@@ -1,13 +1,23 @@
 package groupid.artifactid;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ArtifactidApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ArtifactidApplication.class, args);
+	}
+	@Bean
+	CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+		return args -> {
+			Student maria = new Student();
+			studentRepository.save(maria);
+		};
 	}
 
 }
