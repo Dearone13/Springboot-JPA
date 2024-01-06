50eb195bb2e98ff7500e23248c9f16bf7a717970
Carolina Segura <carosegural13@gmail.com>
2024-01-05 21:09:41 -0500

    Crud hasta count

--- src/main/java/groupid/artifactid/ArtifactidApplication.java
+++ src/main/java/groupid/artifactid/ArtifactidApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class ArtifactidApplication {
 
@@ -15,8 +17,18 @@ public class ArtifactidApplication {
 	@Bean
 	CommandLineRunner commandLineRunner(StudentRepository studentRepository){
 		return args -> {
+			//Creo a ambos estudiantes
 			Student maria = new Student("Maria","Gonzales","maria.gonzales@dearone.edu",21);
 			studentRepository.save(maria);
+
+			Student pedro = new Student("Pedro","Hernandez","pedro.her@unimi.edu",21);
+			System.out.println("Agregando a maria y pedro");
+			//Guardamos una lista de entidades y List of es una lista inmutable
+			studentRepository.saveAll(List.of(maria,pedro));
+			//Numero de estudiantes
+			System.out.println("NUmero de estudinates");
+			//Devuelve el numero total de registros de una tabla
+			System.out.println(studentRepository.count());
 		};
 	}
 
