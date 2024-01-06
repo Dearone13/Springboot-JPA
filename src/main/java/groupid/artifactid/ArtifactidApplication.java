9fb5af83fe550f6243854e8ee4634619f8bea2ce
Carolina Segura <carosegural13@gmail.com>
2024-01-05 22:28:48 -0500

    Main

--- src/main/java/groupid/artifactid/ArtifactidApplication.java
+++ src/main/java/groupid/artifactid/ArtifactidApplication.java
@@ -30,33 +30,6 @@ public class ArtifactidApplication {
 			System.out.println("NUmero de estudinates");
 			//Devuelve el numero total de registros de una tabla
 			System.out.println(studentRepository.count());
-
-			//Se utiliza en lambda para referenciar al metodo
-			//3L ----> NUmero ID tipo long
-			//ifPresentOrElse es un método introducido en Java 9 en la interfaz Optional.
-			//Toma dos argumentos: un consumidor (Consumer) que se ejecutará si el valor
-			// está presente y un Runnable que se ejecutará si el valor no está presente.
-			//() -> Es una expersión lambda si el estudinate no esta presente
-			studentRepository.findById(3L).ifPresentOrElse(System.out::println,()-> System.out.println("Estudinate con id 3 no encontrado"));
-			studentRepository.findById(4L).ifPresentOrElse(System.out::println,()-> System.out.println("Estudinate con id 4 no encontrado"));
-
-
-			//Selecciona a todos los estudiantes
-			System.out.println("Selecciona a todos los estudiantes");
-			List<Student> students = studentRepository.findAll();
-			//Arroga solo objetos y referencia
-			students.forEach(System.out::println);
-			//Imprime información
-			System.out.println("Imprime información");
-			for (Student stu: students) {
-				System.out.println(stu.toString());
-			}
-			System.out.println("Borrar a maria");
-			//Elimina al objeto por id de tipo long
-			studentRepository.deleteById(3L);
-
-			System.out.println("Cantidad actual");
-			System.out.println(studentRepository.count());
 		};
 
 	}
