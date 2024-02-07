e5243405ae7128ef1ab650fd28980e4e731fa59b
Carolina Segura <carosegural13@gmail.com>
2024-02-07 10:47:43 -0500

    Subiendo transacción y persistencia

--- src/main/java/com/hibernate/test/App.java
+++ src/main/java/com/hibernate/test/App.java
@@ -2,6 +2,7 @@ package com.hibernate.test;
 
 import com.hibernate.Model.Employee;
 import javax.persistence.*;
+import java.util.Date;
 import java.util.List;
 
 public class App 
@@ -21,6 +22,26 @@ public class App
         //Se nombra con el nombre de la clase
         List<Employee> employees = manager.createQuery("FROM Employee").getResultList();
         System.out.println("En esta base de datos hay: "+employees.size() +" empleados");
+        Employee e = new Employee(123L,"Karla","Suarez", new Date());
+        Employee e2 = new Employee(124L,"Martha","Hernandez", new Date());
+
+        //Transacción --> Es una unidad de trabajo que agrupa una serie de operaciones en la base de datos.
+        //Inicio la transacción de gestor de persistencia
+        manager.getTransaction().begin();
+        //Guarda la instancia de la entidad en la base de datos
+        manager.persist(e);
+        manager.persist(e2);
+        manager.getTransaction().commit();
+        //Finaliza la transacción del gestor de persistencia
+        showAll();
+    }
+
+    public static void showAll(){
+        List<Employee> employees = (List<Employee>) manager.createQuery("FROM Employee").getResultList();
+        System.out.println("Now: "+ employees.size());
+        for (Employee e: employees) {
+            System.out.println("Employee: "+e.toString());
+        }
 
     }
 }
